b30b279e165b0062eff374ce959086436af58b55
Mehrab Hasan Eshan <2016mehrab@gmail.com>
2024-04-13 22:14:06 +0600

    Added and tested librarianInfo service and controller.

---
+++ src/main/java/com/eshan/library/controllers/LibrarianInfoController.java
@@ -0,0 +1,82 @@
+package com.eshan.library.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eshan.library.models.LibrarianInfo;
+import com.eshan.library.services.LibrarianInfoService;
+
+
+@RestController
+@RequestMapping("/librarianinfo")
+public class LibrarianInfoController {
+    private final LibrarianInfoService librarianInfoService;
+
+    public LibrarianInfoController(LibrarianInfoService librarianInfoService) {
+        this.librarianInfoService = librarianInfoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<LibrarianInfo> post(@RequestBody LibrarianInfo librarianInfo) {
+        try {
+            LibrarianInfo savedLI = librarianInfoService.saveLibrarianInfo(librarianInfo);
+            return new ResponseEntity<LibrarianInfo>(savedLI, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("{libId}")
+    public LibrarianInfo findById(@PathVariable("libId") Integer id) {
+        return librarianInfoService.findLibrarianInfoById(id);
+    }
+
+    @GetMapping
+    public List<LibrarianInfo> findAllStudentInfo() {
+        return librarianInfoService.findAllLibrarianInfo();
+    }
+
+    @DeleteMapping("{libId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteStudentInfo(@PathVariable("libId") Integer id) {
+        librarianInfoService.deleteLibrarianInfo(id);
+    }
+
+    @PutMapping("email/{libId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<LibrarianInfo> updateEmail(@RequestBody LibrarianInfo librarianInfo,
+            @PathVariable("libId") Integer id) {
+        try {
+            librarianInfoService.updateLibrarianInfoEmail(librarianInfo, id);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("name/{libId}")
+    public ResponseEntity<LibrarianInfo> updateName(@RequestBody LibrarianInfo librarianInfo,
+            @PathVariable("libId") Integer id) {
+        try {
+            librarianInfoService.updateLibrarianInfoName(librarianInfo, id);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
---
+++ src/main/java/com/eshan/library/services/LibrarianInfoService.java
@@ -0,0 +1,61 @@
+package com.eshan.library.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.eshan.library.models.Admin;
+import com.eshan.library.models.LibrarianInfo;
+import com.eshan.library.repositories.AdminRepository;
+import com.eshan.library.repositories.LibrarianInfoRepository;
+
+@Service
+public class LibrarianInfoService {
+    private final LibrarianInfoRepository librarianInfoRepository;
+    private final AdminRepository adminRepository;
+    public LibrarianInfoService(LibrarianInfoRepository librarianInfoRepository, AdminRepository adminRepository) {
+        this.librarianInfoRepository = librarianInfoRepository;
+        this.adminRepository = adminRepository;
+    }
+    public LibrarianInfo saveLibrarianInfo(LibrarianInfo librarianInfo){
+        Admin admin = adminRepository.findById(librarianInfo.getAdmin().getId()).orElse(null);
+        if(librarianInfoRepository.existsById(librarianInfo.getId())){
+            throw new RuntimeException("Operation was not successful: Librarian exists");
+        }
+        if (admin != null) {
+            librarianInfo.setAdmin(admin);
+            return librarianInfoRepository.save(librarianInfo);
+        } else {
+            throw new RuntimeException("Operation was not successful: Admin not found");
+        }
+    }
+    public LibrarianInfo findLibrarianInfoById(Integer id){
+        return librarianInfoRepository.findById(id).orElse(null);
+    }
+    public List<LibrarianInfo> findAllLibrarianInfo(){
+        return librarianInfoRepository.findAll();
+    }
+    public void deleteLibrarianInfo(Integer id){
+        librarianInfoRepository.deleteById(id);
+    }
+    public void updateLibrarianInfoEmail(LibrarianInfo librarianInfo, Integer id){
+        LibrarianInfo updatedLI= librarianInfoRepository.findById(id).orElse(null);
+        if(librarianInfo!=null){
+            updatedLI.setEmail(librarianInfo.getEmail());
+            librarianInfoRepository.save(updatedLI);
+        }else {
+            throw new RuntimeException("Operation was not successful: Librarian Not Found!");
+        }
+    }
+
+    public void updateLibrarianInfoName(LibrarianInfo librarianInfo, Integer id){
+        LibrarianInfo updatedLI= librarianInfoRepository.findById(id).orElse(null);
+        if(librarianInfo!=null){
+            updatedLI.setName(librarianInfo.getName());
+            librarianInfoRepository.save(updatedLI);
+        }else {
+            throw new RuntimeException("Operation was not successful: Librarian Not Found!");
+        }
+    }
+
+}
